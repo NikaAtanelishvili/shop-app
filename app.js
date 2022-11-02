b3970149b8561181eafe186c5ba75bbece24f961
NikaAtanelishvili <atanelishvilinika@gmail.com>
2022-11-02 17:57:49 +0400

    added pug (templating engine)

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const shopRoutes = require('./routes/shop')
 
 const app = express()
 
+app.set('view engine', 'pug')
+app.set('views', 'views') // where to find
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
@@ -15,7 +18,7 @@ app.use('/admin', adminData.routes)
 app.use(shopRoutes)
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', 'page-not-found.html'))
+  res.status(404).render('page-not-found')
 })
 
 app.listen(3000)
--- routes/admin.js
+++ routes/admin.js
@@ -7,12 +7,12 @@ const products = []
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'))
+  res.render('add-product', { pageTitle: 'Add Product' })
 })
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-  products.push({title: req.body.title})
+  products.push({ title: req.body.title })
   res.redirect('/')
 })
 
