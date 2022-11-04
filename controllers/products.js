29e9b3ce0df73e7f85d241d50dd8d9e071125b83
NikaAtanelishvili <atanelishvilinika@gmail.com>
2022-11-04 17:29:26 +0400

    Added Products model

--- controllers/products.js
+++ controllers/products.js
@@ -1,29 +1,30 @@
-const products = []
+const Product = require('../models/product')
 
-// admin
 exports.getAddProduct = (req, res, next) => {
   res.render('add-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
     formsCSS: true,
     productCSS: true,
-    activeAddProducts: true,
+    activeAddProduct: true,
   })
 }
 
 exports.postAddProduct = (req, res, next) => {
-  products.push({ title: req.body.title })
+  const product = new Product(req.body.title)
+  product.save()
   res.redirect('/')
 }
 
-// shop
 exports.getProducts = (req, res, next) => {
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/',
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCss: true,
+  Product.fetchAll(products => {
+    res.render('shop', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true,
+    })
   })
 }
