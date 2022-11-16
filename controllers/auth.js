ac94816bfdf0e5127a641681d9c66a593d68e643
NikaAtanelishvili <atanelishvilinika@gmail.com>
2022-11-16 12:46:59 +0400

    Added password reset

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,18 @@
+const crypto = require('crypto')
+
 const bcrypt = require('bcryptjs')
-// sending emails
 const nodemailer = require('nodemailer')
 const sendGridTransport = require('nodemailer-sendgrid-transport')
 
 const User = require('../models/user')
+const SENDGRID_API_KEY = require('../config/config')
+const SENDER_EMAIL = require('../config/config')
 
 // nodemailer setup
 const transporter = nodemailer.createTransport(
   sendGridTransport({
     auth: {
-      api_key:
-        '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',
+      api_key: SENDGRID_API_KEY,
     },
   })
 )
@@ -64,7 +66,7 @@ exports.postLogout = (req, res, next) => {
 
 // sign up
 exports.getSignup = (req, res, next) => {
-  let message = req.flash('signupError')
+  let message = req.flash('error')
 
   message = message.length > 0 ? message[0] : null
   console.log(message)
@@ -83,7 +85,7 @@ exports.postSignup = async (req, res, next) => {
   // if email is taken
   const userDoc = await User.findOne({ email: email })
   if (userDoc) {
-    req.flash('signupError', 'This E-mail is already registered.')
+    req.flash('error', 'This E-mail is already registered.')
     return res.redirect('/signup')
   }
 
@@ -100,11 +102,61 @@ exports.postSignup = async (req, res, next) => {
   await transporter
     .sendMail({
       to: email,
-      from: 'atanelishvilinika@gmail.com',
+      from: SENDER_EMAIL,
       subject: 'Signup secceeded!',
       html: '<h1>You successfully signed up!</h1>',
     })
+    .then(res => console.log(res))
     .catch(err => console.log(err))
 
   res.redirect('/login')
+}
+
+// reset password
+
+exports.getReset = (req, res, next) => {
+  let message = req.flash('error')
+  message = message.length > 0 ? message[0] : null
+
+  res.render('auth/reset', {
+    path: '/reset',
+    pageTitle: 'Reset Password',
+    errorMessage: message,
+  })
+}
+
+exports.postReset = (req, res, next) => {
+  console.log(SENDER_EMAIL, SENDGRID_API_KEY)
+  crypto.randomBytes(32, async (err, buffer) => {
+    if (err) {
+      console.log(err)
+      return res.redirect('/reset')
+    }
+    const token = buffer.toString('hex')
+    const user = await User.findOne({ email: req.body.email })
+
+    if (!user) {
+      req.flash('error', 'No account with that email found.')
+      return res.redirect('/reset')
+    }
+    user.resetToken = token
+    user.resetTokenExpiration = Date.now() + 3600000
+
+    await user.save()
+
+    console.log(req.body.email)
+
+    transporter
+      .sendMail({
+        to: req.body.email,
+        from: SENDER_EMAIL,
+        subject: 'Password reset',
+        html: `
+      <p>You requested a password reset</p>
+      <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password.</p>
+      `,
+      })
+      .catch(err => console.log(err))
+    return res.redirect('/')
+  })
 }
